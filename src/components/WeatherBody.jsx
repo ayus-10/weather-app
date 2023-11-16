02f873a567e0ff73dfe98209525e2cc51bfb57c8
Aayush <136055230+ayus-10@users.noreply.github.com>
2023-11-16 20:20:18 +0545

    implemented the weather api

--- src/components/WeatherBody.jsx
+++ src/components/WeatherBody.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   TiWeatherCloudy,
   TiWeatherShower,
@@ -10,14 +10,35 @@ import {
 import "./WeatherBody.css";
 
 const WeatherBody = () => {
-  const navItems = ["item1", "item2", "item3", "item4", "item5"];
+  const navItems = ["Weather Stats", "Other Stats", "Options"];
+
+  const weatherStats = [
+    "Temperature",
+    "Feels like",
+    "Max temperature",
+    "Min temperature",
+  ];
+
+  const [weatherData, setWeatherData] = useState({});
+
+  const getWeather = () => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9e29402da1d0377e04d1e1503b4d8046&units=metric"
+    )
+      .then((response) => response.json())
+      .then((json) => setWeatherData(json));
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
 
   return (
     <div className="weather-body">
       <div className="weather-summary">
         <div className="weather-image">
           <TiWeatherCloudy size={80} />
-          <span>{"Cloudy"}</span>
+          {weatherData.weather && <span>{weatherData.weather[0].main}</span>}
         </div>
       </div>
       <div className="weather-details">
@@ -27,6 +48,23 @@ const WeatherBody = () => {
               {navItem}
             </span>
           ))}
+        </div>
+        <div className="weather-stats">
+          <ul>
+            {weatherStats.map((weatherStat, index) => (
+              <li key={index}>{weatherStat}</li>
+            ))}
+          </ul>
+          <ul>
+            {weatherData.main && (
+              <>
+                <li>{weatherData.main.temp}</li>
+                <li>{weatherData.main.feels_like}</li>
+                <li>{weatherData.main.temp_max}</li>
+                <li>{weatherData.main.temp_min}</li>
+              </>
+            )}
+          </ul>
         </div>
       </div>
     </div>
